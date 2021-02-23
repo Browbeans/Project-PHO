f7ba78898ec5b2f963d1f970c0c977c98961fad2
Oliver Johansson <oliverjohansson3@gmail.com>
2021-02-23 11:12:30 +0100

    Filestructure and h1

--- weather-app/src/components/FooterContent.tsx
+++ weather-app/src/components/FooterContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 
 function FooterContent() {
-
+    
 }
 
 export default FooterContent; 
--- weather-app/src/components/Layout.tsx
+++ weather-app/src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 
 function Layout() {
-
+    return <h1>HEJSAN</h1>
 }
 
 export default Layout; 
