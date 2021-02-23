ce95b21185b30c9a22bd83634b1c22c2c4be967c
Oliver Johansson <oliverjohansson3@gmail.com>
2021-02-23 11:00:06 +0100

    Filestructure

---
+++ weather-app/src/components/FooterContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'; 
+
+function FooterContent() {
+
+}
+
+export default FooterContent; 
---
+++ weather-app/src/components/MainContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'; 
+
+function MainContent() {
+
+}
+
+export default MainContent; 
