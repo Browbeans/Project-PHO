af7a4fb2ce1ec332819f5f44b3ade7c4b3c2e1f2
Browbeans <oliverjohansson3@gmail.com>
2021-03-01 15:16:04 +0100

    Dynamic objects

--- weather-app/src/components/MainContent.tsx
+++ weather-app/src/components/MainContent.tsx
@@ -5,14 +5,14 @@ import BarItem from './BarItem';
 import BarNavigation from './BarNavigation';
 import ContentItem from './ContentItem';
 
-const imageAvenyn =
-  "https://www.avenyn.se/wp-content/uploads/2016/11/avenyvandring700x350.jpg";
+// const imageAvenyn =
+//   "https://www.avenyn.se/wp-content/uploads/2016/11/avenyvandring700x350.jpg";
 
-  const imageHisingen =
-    "https://cached-images.bonnier.news/swift/bilder/epi-30-dn/UploadedImages/2016/3/29/283a28b8-4979-4d3b-9ebc-3b515b01299f/bigOriginal.jpg";
+//   const imageHisingen =
+//     "https://cached-images.bonnier.news/swift/bilder/epi-30-dn/UploadedImages/2016/3/29/283a28b8-4979-4d3b-9ebc-3b515b01299f/bigOriginal.jpg";
 
-const imageJarntorget =
-  "https://www.gp.se/image/policy:1.23707940:1581244196/image.jpg?f=Wide%26w=1200%26%24p%24f%24w=834285a";
+// const imageJarntorget =
+//   "https://www.gp.se/image/policy:1.23707940:1581244196/image.jpg?f=Wide%26w=1200%26%24p%24f%24w=834285a";
 
   interface Props{
     
@@ -23,6 +23,14 @@ const imageJarntorget =
   
     // const jarntorget = ['Soho', 'Cafe-publik', 'Brygghuset', 'Hops']
     // const hisingen = ['Ölstugan', 'Kville-Biljard', 'Antibarbarus', 'Lindholmen-Streetfood']
+
+    const data = [
+      {area: 'Avenyn'}, 
+      {area: 'Järntorget'},
+      {area: 'Hisingen'}
+    ]
+
+
     const avenyn = [ {
       title: 'Yaki-da', 
       image: '',
@@ -91,17 +99,20 @@ const imageJarntorget =
       <div style={rootStyle}>
         <Switch>
             <Route exact path="/">
-              <Link to="/avenyn" style={{ textDecoration: "none" }}>
-                <ContentItem title="Avenyn" image={imageAvenyn} />
-              </Link>
-              <Link to="/jarntorget" style={{ textDecoration: "none" }}>
-                <ContentItem title="Järntorget" image={imageJarntorget} />
-              </Link>
-              <Link to="/hisingen" style={{ textDecoration: "none" }}>
-                <ContentItem title="Hisingen" image={imageHisingen} />
-              </Link> 
+              {data.map((value) => 
+                  <ContentItem title={value.area} key={value.area}/>
+              )}
             </Route>
-            <Route path="/jarntorget">
+            
+            {/* {data.map((value) => 
+              <Route path={'/' + value.area}>
+                <BarNavigation id={value.area} key={value.area}/>
+              </Route>
+            )} */}
+                
+
+
+            <Route path="/järntorget">
                 {jarntorget.map((value) => <BarNavigation id={value.title} key={value.title}/>)}
             </Route> 
             <Route path="/hisingen">
@@ -110,7 +121,7 @@ const imageJarntorget =
             <Route path="/avenyn">
                 {avenyn.map((value) => <BarNavigation id={value.title} key={value.title}/>)}
             </Route>
-            
+
             {avenyn.map((value) => 
               <Route path={'/' + value.title}>
                 <BarItem bar={value} />
