9953be89c36a8c91996c2fd05defb8dc1123dbfc
Oliver Johansson <oliverjohansson3@gmail.com>
2021-03-02 09:39:38 +0100

    Add correct text for bar

--- weather-app/src/components/MainContent.tsx
+++ weather-app/src/components/MainContent.tsx
@@ -34,64 +34,64 @@ import ContentItem from './ContentItem';
     const avenyn = [ {
       title: 'Yaki-da', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Yaki-Da har många ansikten och skepnader. Här finns en terrass, två dansgolv och en prisbelönt kaffebar, utspritt över flera våningar. Högst upp ligger den ekologiska och KRAV-märkta bistron belägen bland takåsarna.'
     },
     {
       title: 'Lounge', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Med flera år på nacken är Lounge(s) en av Avenyns mest etablerade nattklubbar. På flera våningar finns allt som behövs för en lyckad utekväll som barer, dansgolv, roulette och en populär takterass.'
     },
     {
       title: 'Valand', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Valands nattklubb är sin egen värld, vackert inramad av storslagna konstverk, glittrande kristallkronor och – party! På våningsplanet finns en ambitiös restaurangdel, flertalet barer, dansgolv och scener. Nöjesupplevelsen börjar med afterwork och fortsätter till sena natten.'
     },
     {
       title: 'Hardrock-cafe', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Vår restaurang är fyra våningar med matservering, 5 barer, nattklubb och shuffleboard. Här hittar du alltid något att göra! Vi har även flera eventytor som lämpar sig för allt från mindre grupper till större företagsevent. Sommartid har vi vår populära uteservering med 120 sittplatser på första parkett mot ett vimlande Avenyn. Tyvärr tillåter vi inte pälsdjur varken på uteserveringen eller inne i restaurangen. Tack för er förståelse. '
     },
   ]
     const jarntorget = [ {
       title: 'Soho', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'På SoHO Beer House erbjuder dem mat som . Ifall det är något som är frestande föreslår vi att ni gör en bokning på den här restaurangen och provar. Restaurangen erbjuder inte online-bordsbeställning. Vi föreslår att ni skickar ett e-post eller ringer för bordsbokning.'
     },
     {
       title: 'Cafe-publik', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Kafé och bar på Göteborgs alternativa nattlivsstråk, Andra Långgatan. Med fokus på öl och gott kaffe, samt en enklare barmeny är Café Publik ett populärt vattenhål för stadens unga. Café Publik togs under 2010 över i ny regi, men har behållit sitt fokus på olika DJ:s nästan varje kväll, trevlig personal och god stämning. Lokalen i en och en halv våning, kompletteras med en liten uteservering på gatan.'
     },
     {
       title: 'Brygghuset', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Hans får en ide, han måste givetvis föra sitt arv vidare, det vill säga brygga öl! Han kommer över en nedgången lokal vid järntorget som han renoverar upp tillsammans med Kristina och startar så småningom Göteborgs första bryggeri. De döper bryggeriet till Jerntorgets Brygghus!'
     },
     {
       title: 'Hops', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Längst ner på Andra Långgatan i hjärtat av Masthuggstorget hittar du ölsorter från världens alla olika hörn. Vårt ölsortiment är väldigt brett och expanderas kontinuerligt. För att uppnå så stor variation som möjligt så försöker vi ta in en så stor variation av öl som möjligt. Allt från olika mikrobryggerier till förslag från gäster, som gör att just din favoritsort eventuellt kan hamna i våra kylar. Nyheter av öl presenteras kontinuerligt på Facebook och Instagram.'
     },
   ]
     const hisingen = [ {
       title: 'Ölstugan', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'På Ölstugan 1892 Tullen Hisingen kan ni bland annat äta Pub, Gastropub. Om ni är sugen på detta föreslår vi dig att boka ett bord och kolla på menyn. Tyvärr är det inte möjligt att boka ett bord online. Nå dem på e-post eller telefon för bordsbokning. Ni kan ringa på +4631165616.'
     },
     {
       title: 'Kville-biljard', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Kville Biljard har funnits på Kvilletorget sedan 1994 och erbjuder en unik, lugn, trygg och gemytlig atmosfär med trevlig personal och personlig service. Här samlas vi för att spela biljard, ta ett parti dart, utmana vännerna i en trekamp för att umgås, koppla av i någon av våra barer och äta en god bit mat. Vi har även livespelningar, DJ:s och dansgolv, casino, shuffleboard, Pub Quiz samt företagsevent.'
     },
     {
       title: 'Antibarbarus', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'ANTIBARBARUS Hajpade kaffebaren Alkemisten har ett syskon, även detta ställe med placering i Kvillebäcken. Krogen fokuserar på hantverk och hållbarhet, med en street food-baserad meny. Även om restaurangen erbjuder matinfluenser från hela världen, hämtar köket sina råvaror från producenter med lokal förankring. Med en varierande meny får stället stor variation, både vad gäller mat och dryck. Främst är det naturviner som gäller, samt öl från mikrobryggerierna.'
     },
     {
       title: 'Lindholmen-streetfood', 
       image: '',
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad excepturi vero accusamus harum labore itaque dicta cupiditate incidunt. Nisi repellat mollitia eligendi veniam saepe labore est! Voluptatibus officia saepe quidem!'
+      info: 'Gatumat, och det är ganska exakt vad det är! Enkelt & gott, opretentiöst men med krav på kvalité och viljan att vara gatusmart kombinerad med en inbjudande attityd! Det som binder oss samman är kärleken till maten, kvalitén i råvarorna och människorna som äter den! Bärs i mängder'
     },
   ]
 
@@ -103,15 +103,7 @@ import ContentItem from './ContentItem';
                   <ContentItem title={value.area} key={value.area}/>
               )}
             </Route>
-            
-            {/* {data.map((value) => 
-              <Route path={'/' + value.area}>
-                <BarNavigation id={value.area} key={value.area}/>
-              </Route>
-            )} */}
                 
-
-
             <Route path="/järntorget">
                 {jarntorget.map((value) => <BarNavigation id={value.title} key={value.title}/>)}
             </Route> 
